9070492175f5416dab02be8a90149488dd6d00cb
Eric S. Raymond <esr@snark.thyrsus.com>
1992-07-22 03:52:22 +0000

    *** empty log message ***

--- lisp/flow-ctrl.el
+++ lisp/flow-ctrl.el
@@ -1,13 +1,14 @@
 ;;; flow-ctrl.el --- help for lusers on cu(1) or ttys with wired-in ^S/^Q flow control
 
+;;; Copyright (C) 1990 Free Software Foundation, Inc.
+;;; Copyright (C) 1991 Kevin Gallagher
+
 ;; Author Kevin Gallagher
 ;; Maintainer: FSF
-;; Last-Modified: 03 Jun 1992
 ;; Adapted-By: ESR
 ;; Keywords: hardware
 
-;;; Copyright (C) 1990 Free Software Foundation, Inc.
-;;; Copyright (C) 1991 Kevin Gallagher
+;;; This file is part of GNU Emacs.
 ;;;
 ;;; GNU Emacs is distributed in the hope that it will be useful, but
 ;;; WITHOUT ANY WARRANTY.  No author or distributor accepts
