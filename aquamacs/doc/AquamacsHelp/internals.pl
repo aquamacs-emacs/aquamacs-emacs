635f170d2272a999ed8a6fd692c0fc1c9aa4c774
David Reitter <david.reitter@gmail.com>
2005-06-24 23:55:53 +0000

    checkin

---
+++ aquamacs/doc/AquamacsHelp/internals.pl
@@ -0,0 +1,38 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/buffers.jpg/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:prefs/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/aquamacs-tex.jpg/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:command/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:shortcuts/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/aquamacs-screenshot.jpg/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:terms/;
+$ref_files{$key} = "$dir".q|node16.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/theme.jpg/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ aquamacs/doc/AquamacsHelp/labels.pl
@@ -0,0 +1,81 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/buffers.jpg/;
+$external_labels{$key} = "$URL/" . q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:prefs/;
+$external_labels{$key} = "$URL/" . q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/aquamacs-tex.jpg/;
+$external_labels{$key} = "$URL/" . q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:command/;
+$external_labels{$key} = "$URL/" . q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:shortcuts/;
+$external_labels{$key} = "$URL/" . q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/aquamacs-screenshot.jpg/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:terms/;
+$external_labels{$key} = "$URL/" . q|node16.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/theme.jpg/;
+$external_labels{$key} = "$URL/" . q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/buffers.jpg/;
+$external_latex_labels{$key} = q|4.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/aquamacs-tex.jpg/;
+$external_latex_labels{$key} = q|5.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:prefs/;
+$external_latex_labels{$key} = q|4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:command/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:shortcuts/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/aquamacs-screenshot.jpg/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/RF1/;
+$external_latex_labels{$key} = q|5.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:terms/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/theme.jpg/;
+$external_latex_labels{$key} = q|5.2|; 
+$noresave{$key} = "$nosave";
+
+1;
+
