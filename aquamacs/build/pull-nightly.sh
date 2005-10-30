e096370f2c577b82ccc9762885b76418051ef12e
David Reitter <david.reitter@gmail.com>
2005-10-30 19:02:41 +0000

    upd

--- aquamacs/build/pull-nightly.sh
+++ aquamacs/build/pull-nightly.sh
@@ -11,45 +11,70 @@ GNUNAME=GNU-Emacs-`date +"%Y-%b-%e-%a"`.dmg.bz2
 NAME=Aquamacs-`date +"%Y-%b-%e-%a"`.tar.bz2
 
 SOURCE=dr@rodrigues.inf.ed.ac.uk:/Users/dr/Aquamacs/builds
-LOG=dr@rodrigues.inf.ed.ac.uk:/Users/dr/Aquamacs/aquamacs-build.log
+LOGPATH=dr@rodrigues.inf.ed.ac.uk:/Users/dr/Aquamacs
 
 DEST=~/web/web/Aquamacs
 
 cd $DEST
 
+
+scp $LOGPATH/cvs-update.log . 2>/dev/null
+scp $LOGPATH/aquamacs-build.log . 2>/dev/null 
+scp $LOGPATH/emacs-build.log . 2>/dev/null
+cp cvs-update.log latest-logs/ 2>/dev/null
+
 mkdir tmp 2>/dev/null
+mkdir latest-logs 2>/dev/null
 
 scp $SOURCE/${NAME} tmp/
 
-echo "<HTML><BODY><span style=\"font-family:sans-serif; font-size:10pt;\">" >latest.html
+
 if [ -e tmp/${NAME} ]; then
+
+    if [ `stat -c %s tmp/${NAME}` -gt 1000000 ]; then
+
 	rm -rf builds
 	mv tmp builds
 	chmod go+rx builds
 	rm Aquamacs-nightly.tar.bz2
 	ln -s builds/$NAME Aquamacs-nightly.tar.bz2
 	echo "The latest Aquamacs Emacs nightly is ${NAME}<BR>" >latest-aquamacs.html
-	GET_LOG="yes"
+	# copy the downloaded log for this step into "latest" because the build worked
+	cp aquamacs-build.log latest-logs/ 2>/dev/null
+
+    else
+	rm -r tmp 
+    fi
 fi
-cat latest-aquamacs.html >>latest.html
+
 
 mkdir gnutmp 2>/dev/null
 scp $SOURCE/${GNUNAME} gnutmp/
 
 if [ -e gnutmp/${GNUNAME} ]; then
+
+    if [ `stat -c %s gnutmp/${GNUNAME}` -gt 1000000 ]; then
+
 	rm -rf gnubuilds
 	mv gnutmp gnubuilds
 	chmod go+rx gnubuilds
 	rm GNU-Emacs-nightly.dmg.bz2 2>/dev/null
 	ln -s gnubuilds/$GNUNAME GNU-Emacs-nightly.dmg.bz2
 	echo "The latest GNU Emacs nightly is ${GNUNAME}" >latest-emacs.html
-	GET_LOG="yes"
+        # copy the downloaded log for this step into "latest" because the build worked
+	cp emacs-build.log latest-logs/  2>/dev/null
+
+    else
+	rm -r gnutmp
+	
+    fi
 fi
-cat latest-emacs.html >>latest.html
+ 
+
+echo "<HTML><BODY><span style=\"font-family:sans-serif; font-size:10pt;\">" >latest.html
+cat latest-aquamacs.html latest-emacs.html >>latest.html
 echo "</span></BODY></HTML>" >>latest.html
 
-if test "$GET_LOG" == "yes"; then
-    scp $LOG . 
-fi
-rm last_night.log  2>/dev/null
-scp $LOG last_night.log
+
+cat latest-logs/cvs-update.log latest-logs/emacs-build.log latest-logs/aquamacs-build.log >latest.log 2>/dev/null
+cat cvs-update.log emacs-build.log aquamacs-build.log >last_night.log 2>/dev/null
