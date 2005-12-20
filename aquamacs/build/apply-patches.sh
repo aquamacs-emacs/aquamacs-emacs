80fdec67cf2a32191ed8ac5852fa393ff3c30d29
David Reitter <david.reitter@gmail.com>
2005-12-20 16:30:51 +0000

    newer version by Terry

--- aquamacs/build/apply-patches.sh
+++ aquamacs/build/apply-patches.sh
@@ -2,61 +2,85 @@
 # start this from the Aquamacs build directory
 # or set environment variables AQUAMACS_ROOT and EMACS_ROOT
 
-# the directory structure must be
-# aquamacs/ Aquamacs CVS checkout
-# emacs / GNU Emacs CVS checkout
+# written by Terry Jones <tcj25@cam.ac.uk>
 
+# Licensed under the GNU Public License, version 2
+
+# This file is part of Aquamacs Emacs.
+
+# the directory structure should be
+# aquamacs/ Aquamacs CVS checkout
+# emacs/ GNU Emacs CVS checkout
+
+# Add your patch file name here (file names cannot contain spaces).
+
+PATCHES="pretty-modeline soft-wrap transparency2
+         calm-startup available-screen mouse-button launch-browser
+         toolbar-button org-gnu-Aquamacs about-aquamacs puresize
+         macfns-dialog-menu reftex menu-bar-visible-frame
+         eval-depth emacs-inline sendmail-mac"
+
+# The patches here are specified separately so their order can be defined.
+
+# It might also be useful(?) to touch a .applied file in the patches
+# directory to indicate which patches have been applied correctly.
+# Then people could re-run apply-patches.sh. You could compare dates
+# and apply the patch file if it were newer than the .applied file
+# in case you picked up changes to the patch file from CVS.
+
+
+# If we are passed any argument, just echo what would be done.
+case $# in
+    0) exec=1 ;;
+    *) exec=0 ;;
+esac
 
 if [ ! ${AQUAMACS_ROOT} ]
 then  
-    AQUAMACS_ROOT=`pwd`/../aquamacs
+    AQUAMACS_ROOT="`/bin/pwd`/../aquamacs"
+fi
+
+if [ ! -d "$AQUAMACS_ROOT" ]
+then
+    echo "$0: your AQUAMACS_ROOT variable (set to '$AQUAMACS_ROOT') does not contain a directory." >&2
+    exit 1
 fi
 
 if [ ! ${EMACS_ROOT} ]
 then
-    EMACS_ROOT=${AQUAMACS_ROOT}/../emacs
+    EMACS_ROOT="$AQUAMACS_ROOT/../emacs"
 fi
 
+if [ ! -d "$EMACS_ROOT" ]
+then
+    echo "$0: your EMACS_ROOT variable (set to '$EMACS_ROOT') does not contain a directory." >&2
+    exit 2
+fi
 
-cd ${EMACS_ROOT} 
-echo "pretty-modeline"
-patch -p0 <${AQUAMACS_ROOT}/patches/pretty-modeline.patch 
-echo "soft-wrap (experimental)"
-patch -p0 <${AQUAMACS_ROOT}/patches/soft-wrap.patch 
-echo "transparency2"
-patch -p0 <${AQUAMACS_ROOT}/patches/transparency2.patch 
-echo "calm-startup"
-patch -p0 <${AQUAMACS_ROOT}/patches/calm-startup.patch 
-echo "available-screen"
-patch -p0 <${AQUAMACS_ROOT}/patches/available-screen.patch
-echo "mouse-button"
-patch -p0 <${AQUAMACS_ROOT}/patches/mouse-button.patch
-echo "launch-browser"
-patch -p0 <${AQUAMACS_ROOT}/patches/launch-browser.patch
-echo "toolbar-button"
-patch -p0 <${AQUAMACS_ROOT}/patches/toolbar-button.patch
-echo "org-gnu-Aquamacs"
-patch -p0 <${AQUAMACS_ROOT}/patches/org-gnu-Aquamacs.patch
-echo "about-aquamacs"
-patch -p0 <${AQUAMACS_ROOT}/patches/about-aquamacs.patch
-echo "puresize"
-patch -p0 <${AQUAMACS_ROOT}/patches/puresize.patch
-echo "macfns-dialog-menu"
-patch -p0 <${AQUAMACS_ROOT}/patches/macfns-dialog-menu.patch
-# reftex bugfix
-echo "reftext bugfix"
-patch -p0 <${AQUAMACS_ROOT}/patches/reftex.patch
-echo "menu-bar-visible-frame"
-patch -p0 <${AQUAMACS_ROOT}/patches/menu-bar-visible-frame.patch
-echo "eval-depth"
-patch -p0 <${AQUAMACS_ROOT}/patches/eval-depth.patch
+cd ${EMACS_ROOT} || exit 3
 
-echo "emacs-inline"
-patch -p0 <${AQUAMACS_ROOT}/patches/emacs-inline.patch
+for patch in $PATCHES
+do
+    file="$AQUAMACS_ROOT/patches/$patch.patch"
 
-# temp
-echo "sendmail-mac"
-patch -p0 <${AQUAMACS_ROOT}/patches/sendmail-mac.patch
+    if [ ! -f "$file" ]
+    then
+	echo "$0: WARNING: patch file '$file' does not exist, skipping." >&2
+	continue
+    fi
 
+    echo "Applying patch: '$patch'"
 
+    if [ $exec -ne 0 ]
+    then
+	patch -p0 < "$file"
+    else
+	echo "patch -p0 < '$file'"
+    fi
 
+    if [ $? -ne 0 ]
+    then
+	# Maybe we should exit here?
+	echo "$0: WARNING: patch for file '$file' failed." >&2
+    fi
+done
