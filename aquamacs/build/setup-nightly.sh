882635f99c6f2e48050f2cb3bb268e3fd3d6a53f
David Reitter <david.reitter@gmail.com>
2006-03-10 17:57:27 +0000

    savannah change

--- aquamacs/build/setup-nightly.sh
+++ aquamacs/build/setup-nightly.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 # initial setup
+ 
+. ~/.bash_profile
 
 mkdir Aquamacs
 cd Aquamacs
 mkdir builds
 
 export CVS_RSH="ssh"
-cvs -z3 -d:ext:anoncvs@savannah.gnu.org:/cvsroot/emacs co -d emacs.raw emacs
+cvs -z3 -d:pserver:anonymous@cvs.savannah.gnu.org:/sources/emacs co -d emacs.raw emacs
 
 cvs -d:pserver:anonymous@cvs.aquamacs.org:/cvsroot/aquamacs login
 cvs -z3 -d:pserver:anonymous@cvs.aquamacs.org:/cvsroot/aquamacs co -P aquamacs
 
+ln -s aquamacs/builds/nightly.sh .
