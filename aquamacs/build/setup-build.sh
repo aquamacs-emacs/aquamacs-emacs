3bb1bf81341caf6eeb8b6725d8980ff2b07e5e3d
David Reitter <david.reitter@gmail.com>
2006-03-11 10:32:53 +0000

    renamed

---
+++ aquamacs/build/setup-build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# initial setup
+ 
+
+mkdir Aquamacs
+cd Aquamacs
+mkdir builds
+
+export CVS_RSH="ssh"
+cvs -z3 -d:pserver:anonymous@cvs.savannah.gnu.org:/sources/emacs co -d emacs.raw emacs
+
+cvs -d:pserver:anonymous@cvs.aquamacs.org:/cvsroot/aquamacs login
+cvs -z3 -d:pserver:anonymous@cvs.aquamacs.org:/cvsroot/aquamacs co -P aquamacs
+
+ln -s aquamacs/builds/nightly.sh build.sh
