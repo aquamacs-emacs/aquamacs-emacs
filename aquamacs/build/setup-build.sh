aa8a0dffed0b50883ee3edf0e2fed455510ea4a6
David Reitter <david.reitter@gmail.com>
2006-03-26 14:31:56 +0000

    read AQUAMACS_CVS_PREFIX if needed

--- aquamacs/build/setup-build.sh
+++ aquamacs/build/setup-build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # initial setup
  
+if test "$AQUAMACS_CVS_PREFIX" ; then
+    CVS_PREFIX=$AQUAMACS_CVS_PREFIX
+else
+    CVS_PREFIX="cvs -z3 -d:pserver:anonymous@cvs.aquamacs.org:/cvsroot/aquamacs"
+fi
+
 
 mkdir Aquamacs
 cd Aquamacs
@@ -9,7 +15,7 @@ mkdir builds
 export CVS_RSH="ssh"
 cvs -z3 -d:pserver:anonymous@cvs.savannah.gnu.org:/sources/emacs co -d emacs.raw emacs
 
-cvs -d:pserver:anonymous@cvs.aquamacs.org:/cvsroot/aquamacs login
-cvs -z3 -d:pserver:anonymous@cvs.aquamacs.org:/cvsroot/aquamacs co -P aquamacs
+#cvs -d:pserver:anonymous@cvs.aquamacs.org:/cvsroot/aquamacs login
+$CVS_PREFIX co -P aquamacs
 
 ln -s aquamacs/build/build.sh build.sh
