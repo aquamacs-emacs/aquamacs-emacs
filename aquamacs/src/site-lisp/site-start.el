88e0de329c60514cd802942d0ff70ff177d62dee
David Reitter <david.reitter@gmail.com>
2006-06-25 18:10:15 +0000

    new version number (major version)

--- aquamacs/src/site-lisp/site-start.el
+++ aquamacs/src/site-lisp/site-start.el
@@ -5,7 +5,7 @@
 ;; Maintainer: David Reitter
 ;; Keywords: aquamacs
  
-;; Last change: $Id: site-start.el,v 1.19 2006/05/01 11:09:31 davidswelt Exp $
+;; Last change: $Id: site-start.el,v 1.20 2006/06/25 18:10:15 davidswelt Exp $
 
 ;; This file is part of Aquamacs Emacs
 ;; http://www.aquamacs.org/
@@ -39,7 +39,7 @@
 
 ;; Version information
 
-(defvar aquamacs-version "0.9.9"
+(defvar aquamacs-version "0.9.9d"
 "A string with Aquamacs' version number.
 The format of the string is undefined. 
 For a reliable numerical representation, use `aquamacs-version-id'.")
@@ -53,7 +53,7 @@ It is guaranteed that iff of two Aquamacs releases A and B,
 B is newer than A, then aquamacs-version-id for B is higher 
 than aquamacs-version-id for A.")
 
-(defvar aquamacs-minor-version "CVS"
+(defvar aquamacs-minor-version ""
 "Version code for minor maintenance releases.
 Changes in this code are ignored during the online version check.")
 
