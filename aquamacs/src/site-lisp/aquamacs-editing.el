45ba403968011330f4324b6966f02d5b0dce35d6
David Reitter <david.reitter@gmail.com>
2007-02-13 12:32:21 +0000

    editing commands (unfill)

---
+++ aquamacs/src/site-lisp/aquamacs-editing.el
@@ -0,0 +1,102 @@
+;; Aquamacs Editing Helper
+;; some editing functions for Aquamacs
+ 
+;; Author: David Reitter, david.reitter@gmail.com
+;; Maintainer: David Reitter
+;; Keywords: aquamacs
+ 
+;; Last change: $Id: aquamacs-editing.el,v 1.1 2007/02/13 12:32:21 davidswelt Exp $
+
+;; This file is part of Aquamacs Emacs
+;; http://www.aquamacs.org/
+
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+ 
+;; Copyright (C) 2005, 2007 David Reitter
+
+
+
+
+
+;; these are taken from wikipedia-mode.el by Chong Yidong, Uwe Brauer
+;; license: GPL2, 2007-02-13
+
+;; slightly modified by David Reitter for Aquamacs
+
+(defun unfill-region () 
+"Undo filling, deleting stand-alone newlines.
+Newlines that do not end paragraphs, list entries, etc. in the region
+are deleted."
+  (interactive)
+  (save-excursion
+	(narrow-to-region (point) (mark))
+    (goto-char (point-min))
+    (while (re-search-forward ".\\(\n\\)\\([^# *;:|!\n]\\|----\\)" nil t)
+      (replace-match " " nil nil nil 1)))
+  (message "Region unfilled. Stand-alone newlines deleted")
+  (widen))
+ 
+(defun unfill-paragraph-or-region () ; Version:1.7dr
+  "Unfill paragraph or region (if any).
+When no region is defined (mark is not active), puts a paragraph
+ (separated by empty lines) in one (long line). If a region is
+defined, acts like `unfill-region'."
+  (interactive)
+
+  (if mark-active
+    (unfill-region)
+  (when use-hard-newlines
+	;; 	(backward-paragraph 1)
+	;; 	(next-line 1)
+	(beginning-of-line 1)
+	(set-fill-prefix)
+	(set (make-local-variable 'use-hard-newlines) nil)
+	(set (make-local-variable 'sentence-end-double-space) t)
+	(set (make-local-variable 'paragraph-start)
+		 "[ ’¡¡	\n]")
+	(when  (featurep 'xemacs)	
+	  (let ((fill-column (point-max)))
+		(fill-paragraph-or-region nil)))
+	(unless  (featurep 'xemacs)	
+	  (let ((fill-column (point-max)))
+		(fill-paragraph nil)))
+	(set (make-local-variable 'use-hard-newlines) t)
+	(set (make-local-variable 'sentence-end-double-space) nil)
+	(set (make-local-variable 'paragraph-start)
+		 "\\*\\| \\|#\\|;\\|:\\||\\|!\\|$"))
+
+  (unless use-hard-newlines
+	;; 	(backward-paragraph 1)
+	;; 	(next-line 1)
+	(beginning-of-line 1)
+	(set-fill-prefix)
+	(set (make-local-variable 'sentence-end-double-space) t)
+	(set (make-local-variable 'paragraph-start)
+		 "[ ’¡¡	\n]")
+	(when  (featurep 'xemacs)	
+	  (let ((fill-column (point-max)))
+		(fill-paragraph-or-region nil)))
+	(unless  (featurep 'xemacs)	
+	  (let ((fill-column (point-max)))
+		(fill-paragraph nil)))
+	(set (make-local-variable 'sentence-end-double-space) nil)
+	(set (make-local-variable 'paragraph-start)
+		 "\\*\\| \\|#\\|;\\|:\\||\\|!\\|$"))))
+ 
+ 
+ 
+(provide 'aquamacs-editing)
