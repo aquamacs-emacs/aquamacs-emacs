3f82130e8cad77f41308f55141616a2e7727693c
David Reitter <david.reitter@gmail.com>
2007-03-31 14:17:50 +0000

    update

--- aquamacs/doc/AquamacsHelp/internals.pl
+++ aquamacs/doc/AquamacsHelp/internals.pl
@@ -3,27 +3,27 @@
 
 
 $key = q/buffers.jpg/;
-$ref_files{$key} = "$dir".q|node38.html|; 
+$ref_files{$key} = "$dir".q|node30.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/tab:variables/;
-$ref_files{$key} = "$dir".q|node43.html|; 
+$ref_files{$key} = "$dir".q|node35.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/aquamacs-tex.jpg/;
-$ref_files{$key} = "$dir".q|node44.html|; 
+$ref_files{$key} = "$dir".q|node36.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/tab:command/;
-$ref_files{$key} = "$dir".q|node42.html|; 
+$ref_files{$key} = "$dir".q|node34.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/Look/;
-$ref_files{$key} = "$dir".q|node37.html|; 
+$ref_files{$key} = "$dir".q|node29.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/tab:shortcuts/;
-$ref_files{$key} = "$dir".q|node42.html|; 
+$ref_files{$key} = "$dir".q|node34.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/aquamacs-screenshot.jpg/;
@@ -31,19 +31,19 @@ $ref_files{$key} = "$dir".q|node2.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/theme.jpg/;
-$ref_files{$key} = "$dir".q|node37.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/tab:terms/;
 $ref_files{$key} = "$dir".q|node29.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/tab:terms/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/frame-appearance-styles/;
-$ref_files{$key} = "$dir".q|node37.html|; 
+$ref_files{$key} = "$dir".q|node29.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/standardpaths/;
-$ref_files{$key} = "$dir".q|node49.html|; 
+$ref_files{$key} = "$dir".q|node41.html|; 
 $noresave{$key} = "$nosave";
 
 1;
