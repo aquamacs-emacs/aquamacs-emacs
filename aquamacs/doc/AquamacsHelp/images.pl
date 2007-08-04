ff0e10d4aa4ee267623ff01cd5fb748a2d54b9e3
David Reitter <david.reitter@gmail.com>
2007-08-04 06:36:10 +0000

    update - newly compiled with new latex2html configuration files

--- aquamacs/doc/AquamacsHelp/images.pl
+++ aquamacs/doc/AquamacsHelp/images.pl
@@ -4,7 +4,7 @@
 
 $key = q/{;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="13" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
+ WIDTH="13" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img1.png"
  ALT="$\{$">|; 
 
@@ -22,13 +22,13 @@ $cached_env_img{$key} = q|<IMG
 
 $key = q/};MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="13" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
+ WIDTH="13" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img5.png"
  ALT="$\}$">|; 
 
 $key = q/backslash;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="13" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
+ WIDTH="13" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img2.png"
  ALT="$\backslash$">|; 
 
