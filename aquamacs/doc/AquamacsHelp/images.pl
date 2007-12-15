6706121f63687ad13a709ede4736ce0fc6f0cfbc
David Reitter <david.reitter@gmail.com>
2007-12-15 22:14:51 +0000

    update for aquamacs 1.3

--- aquamacs/doc/AquamacsHelp/images.pl
+++ aquamacs/doc/AquamacsHelp/images.pl
@@ -2,35 +2,5 @@
 # Associate images original text with physical files.
 
 
-$key = q/{;MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="13" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img1.png"
- ALT="$\{$">|; 
-
-$key = q/>;MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="18" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img4.png"
- ALT="$&gt;$">|; 
-
-$key = q/<;MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="18" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="$&lt;$">|; 
-
-$key = q/};MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="13" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img5.png"
- ALT="$\}$">|; 
-
-$key = q/backslash;MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="13" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="$\backslash$">|; 
-
 1;
 
