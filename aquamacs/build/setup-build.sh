1f21e8fd299eb7f2dbd2679376e13809a11d2e24
David Reitter <david.reitter@gmail.com>
2007-12-20 23:29:24 +0000

    use EMACS_22_BASE branch give instructions at the end

--- aquamacs/build/setup-build.sh
+++ aquamacs/build/setup-build.sh
@@ -13,9 +13,15 @@ cd Aquamacs
 mkdir builds
 
 export CVS_RSH="ssh"
-cvs -z3 -d:pserver:anonymous@cvs.savannah.gnu.org:/sources/emacs co -d emacs.raw emacs
+cvs -z3 -d:pserver:anonymous@cvs.savannah.gnu.org:/sources/emacs co -r EMACS_22_BASE -d emacs.raw emacs
 
 #cvs -d:pserver:anonymous@cvs.aquamacs.org:/cvsroot/aquamacs login
 $CVS_PREFIX co -P aquamacs
 
 ln -s aquamacs/build/build.sh build.sh
+
+cd Aquamacs
+
+echo "Build environment has been created. Type this now to make Aquamacs:"
+echo "./build.sh aquamacs"
+
