a4d4c7a21ced06474793e0cdcd106330e1084add
David Reitter <david.reitter@gmail.com>
2008-12-15 22:17:23 +0000

    1.6preview-1

--- aquamacs/src/site-lisp/site-start.el
+++ aquamacs/src/site-lisp/site-start.el
@@ -5,7 +5,7 @@
 ;; Maintainer: David Reitter
 ;; Keywords: aquamacs
  
-;; Last change: $Id: site-start.el,v 1.57 2008/10/12 23:49:43 davidswelt Exp $
+;; Last change: $Id: site-start.el,v 1.58 2008/12/15 22:17:23 davidswelt Exp $
 
 ;; This file is part of Aquamacs Emacs
 ;; http://www.aquamacs.org/
@@ -37,7 +37,7 @@
 
 ;; Version information
 
-(defvar aquamacs-version "1.6CVS"
+(defvar aquamacs-version "1.6preview"
 "A string with Aquamacs' version number.
 The format of the string is undefined. 
 For a reliable numerical representation, use `aquamacs-version-id'.")
@@ -51,12 +51,12 @@ It is guaranteed that for any two Aquamacs releases A and B,
 if aquamacs-version-id for B is higher than aquamacs-version-id 
 for A, then B is newer than A.")
 
-(defvar aquamacs-minor-version ""
+(defvar aquamacs-minor-version "1"
 "Version code for minor maintenance releases.
 Changes in this code are ignored during the online version check.")
 
 ;; only for Emacs.app
-(when nil
+(when (fboundp 'ns-find-file) ;; running Cocoa?
  (setq load-path (cons "~/src" load-path))
  (setq load-path (cons "~/src/oneonone" load-path))
  (setq load-path (cons "~/src/macosx" load-path))
