aa256f05d591723cdcc21dcbe2b00a94fb3d0415
David Reitter <david.reitter@gmail.com>
2009-04-21 02:31:26 +0000

    nightly build script: builds and copies to web server

---
+++ aquamacs/build/nightly-build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+cd ~/Nightly/Cocoa23/aquamacs-emacs.git
+
+AQUAMACS_ROOT=`pwd`/aquamacs
+
+rm aquamacs-build.log
+date >>aquamacs-build.log
+
+git checkout -f master >>aquamacs-build.log  2>>aquamacs-build.log
+git pull origin master  >>aquamacs-build.log  2>>aquamacs-build.log
+
+./build-aquamacs >>aquamacs-build.log 2>>aquamacs-build.log
+
+date >>aquamacs-build.log
+
+# now we have 
+
+LOG=`pwd`/aquamacs-build.log
+APP=`pwd`/nextstep/Aquamacs.app
+DATE=`date +"%Y-%b-%d-%a-%H%M"`
+
+# zip it up
+
+mkdir builds 2>/dev/null
+tar cjf builds/Aquamacs-${DATE}.tbz ${APP}
+
+# copy to server
+
+aquamacs/build/copy-build-to-server.sh $DATE # $SHORTDATE  - only needed for GNU Emacs
