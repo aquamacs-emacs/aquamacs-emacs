2cd4315580d04e5dba514a1ecf29004f7187859b
David Reitter <david.reitter@gmail.com>
2009-08-05 11:34:20 -0400

    preview2

--- aquamacs/src/site-lisp/site-start.el
+++ aquamacs/src/site-lisp/site-start.el
@@ -37,12 +37,12 @@
 
 ;; Version information
 
-(defvar aquamacs-version "2.0preview1"
+(defvar aquamacs-version "2.0preview2"
 "A string with Aquamacs' version number.
 The format of the string is undefined. 
 For a reliable numerical representation, use `aquamacs-version-id'.")
 
-(defvar aquamacs-version-id 201
+(defvar aquamacs-version-id 202
 "A float indicating Aquamacs' version number.
 Full integers correspond to the third position of the public
 version number, e.g. version 0.9.7 is represented as `97.x'.
