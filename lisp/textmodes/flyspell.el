aquamacs-emacs/aquamacs-emacs

commit 2b751b4acc1d26893b4417c79c02fcc160e06994

honor nil result of ns-spellchecker-check-spelling

lisp/textmodes/flyspell.el
@@ -294,25 +294,35 @@ effect, marks the misspelled word (if found) with face flyspell-incorrect."
 	misspell-location
 	misspell-end
 	done)
+    ;; update dictionary if needed
     (unless (string= ispell-current-dictionary
 		     (ns-spellchecker-current-language))
       (ispell-change-dictionary (ns-spellchecker-current-language)))
+    ;; loop through until we find a misspelling or end of string
     (while (not done)
       (setq misspell-location
 	    (ns-spellchecker-check-spelling (buffer-substring pos end)
-					    (current-buffer))
-	    misspell-end (+ pos (car misspell-location) (cdr misspell-location)))
-      (if (= (car misspell-location) -1)
-	  (setq done t)
-	(save-excursion
-	  (goto-char misspell-end)
-	  (if (flyspell-word)
-	      (setq misspell-location nil)
-	    (setq done t))))
+					    (current-buffer)))
+      (if misspell-location
+	  (save-excursion
+	    (setq misspell-end
+		  (+ pos (car misspell-location) (cdr misspell-location)))
+	    (goto-char misspell-end)
+	    (if (flyspell-word) ;; returns t if not misspelled
+		;; ignore misspelling if flyspell-word says it's OK,
+		;;  but continue checking
+		(setq misspell-location nil)
+	      ;; if flyspell-word concurs, we've found a misspelling & are done
+	      (setq done t)
+	      ))
+	;; no misspellings in string; finish.
+	(setq done t))
       (unless done
-	  (setq pos misspell-end)))
-    (if (= (car misspell-location) -1)
-	nil
+	;; check remainder of string
+	(if misspell-location
+	    (setq pos misspell-end))))
+    ;; if a misspelling has been found, report is location (otherwise nil)
+    (when misspell-location
       (cons (+ pos (car misspell-location)) misspell-end))
     ))
   
@@ -346,9 +356,8 @@ the current highlighted word (if any)."
       ;;   is ignored or learned in the spelling panel
       (let ((word (word-at-point)))
 	(if (and word
-		 (eq (car (ns-spellchecker-check-spelling word (current-buffer)))
-		     -1))
-	  (flyspell-unhighlight-at (point))))
+		 (not (ns-spellchecker-check-spelling word (current-buffer))))
+	    (flyspell-unhighlight-at (point))))
       ;; If midway through a word, start at search at next word;
       ;;   but don't skip an entire word
       (if (backward-word)
@@ -521,9 +530,9 @@ on current value of `ispell-program-name'."
 	(setq spellcheck-text (buffer-substring spellcheck-position end)) 
 	;; find (position . length) of first misspelled word in extracted text
 	(setq ns-spellcheck-output 
-	      ;; returns (-1 . 0) if no misspellings found
+	      ;; returns nil if no misspellings found
 	      (ns-spellchecker-check-spelling spellcheck-text (current-buffer)))
-	(if (>= (car ns-spellcheck-output) 0)
+	(if ns-spellcheck-output
 	    ;; found misspelled word
 	    (progn
 	      (setq misspelled-location

lisp/textmodes/ispell.el
@@ -870,15 +870,14 @@ and Return:
 		     (ns-spellchecker-current-language))
       (ispell-change-dictionary (ns-spellchecker-current-language)))
   (let* ((output (ns-spellchecker-check-spelling word (current-buffer)))
-	 (offset (1+ (car output))))
-    (cond
-     ;; word is correct -- return t
-     ((equal output (cons -1 0)) t)
-     ;; word is incorrect -- return
-     ;; (\"ORIGINAL-WORD\" OFFSET MISS-LIST GUESS-LIST)
-     ;; don't know what the difference between miss-list and guess-list is...
-     ((> offset 0)
-      (list word offset (ns-spellchecker-get-suggestions word) nil)))))
+	 (offset (car output)))
+    (if offset
+	;; word is incorrect -- return
+	;; (\"ORIGINAL-WORD\" OFFSET MISS-LIST GUESS-LIST)
+	;; GUESS-LIST built from known affixes is nil for NSSpellChecker
+	(list word (+1 offset) (ns-spellchecker-get-suggestions word) nil)
+      ;; offset is nil: word is correct -- return t
+      t)))
 
 (defun ispell-ns-spellcheck-string (string) 
   "NSSpellChecker replacement for ispell-parse-output.  Spellcheck STRING
@@ -902,9 +901,8 @@ and return a list of lists (one for each misspelled word) of the format:
 		   (ns-spellchecker-check-spelling string (current-buffer))
 		   offset (car ns-spellcheck-output)
 		   length (cdr ns-spellcheck-output))
-	     (if (< offset 0)
-		 ;; no misspelled words -- terminate while loop
-		 nil
+	     ;; if no misspelled words, terminate while loop
+	     (when offset
 	       ;; misspelled word found; get word;
 	       ;;  set string to not-yet-checked portion;
 	       ;;  add details of misspelling to head of return-list

commit e833fe1a018c892cbaeaaa4b1555a22d9a25694d

fix spellcheck bug introduced in 2b751b4acc1d26893b4417c79c02fcc160e06994

lisp/textmodes/flyspell.el
@@ -319,8 +319,7 @@ effect, marks the misspelled word (if found) with face flyspell-incorrect."
 	(setq done t))
       (unless done
 	;; check remainder of string
-	(if misspell-location
-	    (setq pos misspell-end))))
+	(setq pos misspell-end)))
     ;; if a misspelling has been found, report is location (otherwise nil)
     (when misspell-location
       (cons (+ pos (car misspell-location)) misspell-end))