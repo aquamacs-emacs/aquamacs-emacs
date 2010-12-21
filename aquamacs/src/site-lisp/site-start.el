8ddc234ac44ffaaa4a5ada240de4d687b1230f25
David Reitter <david.reitter@gmail.com>
2010-12-20 20:59:30 -0500

    Aquamacs/24: set version number to 3.x

--- aquamacs/src/site-lisp/site-start.el
+++ aquamacs/src/site-lisp/site-start.el
@@ -37,12 +37,12 @@
 
 ;; Version information
 
-(defvar aquamacs-version "2.2"
+(defvar aquamacs-version "3.x"
 "A string with Aquamacs' version number.
 The format of the string is undefined. 
 For a reliable numerical representation, use `aquamacs-version-id'.")
 
-(defvar aquamacs-version-id 211
+(defvar aquamacs-version-id 300
 "A float indicating Aquamacs' version number.
 Full integers correspond to the third position of the public
 version number, e.g. version 0.9.7 is represented as `97.x'.
