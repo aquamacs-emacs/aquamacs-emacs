469abba8416cad25b7fbf7c91e188591984f2ded
David Reitter <david.reitter@gmail.com>
2011-07-27 14:50:57 -0400

    build scripts: provide "release" option. The non-release is deemed local and doesn't contain zipped files, but it links against local libraries.

--- aquamacs/build/build23.sh
+++ aquamacs/build/build23.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
-# do not use MacPorts / fink libraries
-# during development, do not compress .el files to speed up
+OMIT_AUTOGEN=1
 
-PATH=/bin:/sbin:/usr/bin  GZIP_PROG=  ./configure --with-ns --without-x
-make bootstrap
+if [ "$1" = "-release" ];
+then  
+  # do not use MacPorts / fink libraries for binary compatibility
+  export PATH=/bin:/sbin:/usr/bin   
+  export GZIP_PROG=`which gzip`
+  echo "Building Aquamacs (release)."
+  OMIT_AUTOGEN=
+else
+  # during development, do not compress .el files to speed up the build
+  export GZIP_PROG=
+  echo "Building Aquamacs (development)."
+
+  if [ ! -e "configure" ];
+  then
+    OMIT_AUTOGEN=
+  fi
+fi
+
+test $OMIT_AUTOGEN || ./autogen.sh ; \
+./configure --with-ns --without-x ; \
+make bootstrap ; \
 make install
