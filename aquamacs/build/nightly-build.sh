9e32bec3d514de974c6b705c0c1d035d895562bc
David Reitter <david.reitter@gmail.com>
2011-08-01 23:31:19 -0400

    nightly build: stop if error occurs.  build documentation. more progress output.

--- aquamacs/build/nightly-build.sh
+++ aquamacs/build/nightly-build.sh
@@ -8,28 +8,33 @@ EMACS_ROOT=`pwd`
 AQUAMACS_ROOT=`pwd`/aquamacs
 
 rm aquamacs-build.log
+echo "Begin building Aquamacs." >>aquamacs-build.log
 date >>aquamacs-build.log
+
+echo "Updating working directory from Git repository." >>aquamacs-build.log
 
 git checkout -f aquamacs24 >>aquamacs-build.log  2>>aquamacs-build.log
 git pull origin aquamacs24  >>aquamacs-build.log  2>>aquamacs-build.log
 
-aquamacs/build/build24.sh -release >>aquamacs-build.log 2>>aquamacs-build.log
+echo "Building Aquamacs documentation." >>aquamacs-build.log
 
-date >>aquamacs-build.log
+# update documentation: requires latex (tetex with nonfreefonts package)
+PATH=/usr/texbin/:/usr/local/bin/:$PATH make 2>>../../../aquamacs-build.log
+cd -
 
-# now we have 
+echo "Building Aquamacs (incremental build)." >>aquamacs-build.log
 
 LOG=`pwd`/aquamacs-build.log
 APP=`pwd`/nextstep/Aquamacs.app
 DATE=`date +"%Y-%b-%d-%a-%H%M"`
 BLD=`pwd`/builds/Aquamacs-${DATE}.tar.bz2
-# zip it up
 
-mkdir builds 2>/dev/null
-cd `dirname ${APP}`
-tar cjf ${BLD} Aquamacs.app
-
-# copy to server
-
-cd ${EMACS_ROOT}
+# one step builds on the next:
+aquamacs/build/build24.sh -release >>aquamacs-build.log 2>>aquamacs-build.log ; \
+date >>aquamacs-build.log ; \
+echo "Packaging Aquamacs." >>aquamacs-build.log ; \
+mkdir builds 2>/dev/null ; \
+cd `dirname ${APP}` ; \
+tar cjf ${BLD} Aquamacs.app ; \
+cd ${EMACS_ROOT} ; \
 aquamacs/build/copy-build-to-server.sh $DATE # $SHORTDATE  - only needed for GNU Emacs
