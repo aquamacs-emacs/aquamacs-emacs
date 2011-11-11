ed176c91162fbea9d286cbc7d922671ca7af9147
David Reitter <david.reitter@gmail.com>
2011-11-11 00:21:41 -0500

    Implement full Lion full-screen support Impl EmacsWindow toggleActualFullScreen to call NSWindow's full-screen code (with transition animation). Call from ns_fullscreen_hook when warranted. impl EmacsView willUseFullScreenPresentationOptions:, window:willUseFullScreenContentSize:, and windowDidExitFullScreen: akin to whwt would otherwise be done in ns_fullscreen_hook.  Refactoring needed.
    Authors: D.R. and Daisuke Murase

--- src/nsterm.h
+++ src/nsterm.h
@@ -118,7 +118,8 @@ along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
 {
   NSPoint grabOffset;
 }
-
+- (BOOL) respondsToNativeFullScreen;
+- (BOOL)isFullScreen;
 -(EmacsWindow *)setFullscreen: (BOOL) flag;
 - (void)toggleToolbarShown: (id)sender;
 - (void)toggleFullScreen: (id)sender;
