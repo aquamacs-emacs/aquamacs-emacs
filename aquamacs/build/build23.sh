cc48072f43bb43531811197f06e160a245d06ef3
David Reitter <david.reitter@gmail.com>
2012-05-30 15:33:35 -0400

    build script: add /usr/sbin to path for	chown. GH issue 67

--- aquamacs/build/build23.sh
+++ aquamacs/build/build23.sh
@@ -3,6 +3,6 @@
 # do not use MacPorts / fink libraries
 # during development, do not compress .el files to speed up
 
-PATH=/bin:/sbin:/usr/bin  GZIP_PROG=  ./configure --with-ns --without-x
+PATH=/bin:/sbin:/usr/bin:/usr/sbin  GZIP_PROG=  ./configure --with-ns --without-x
 make bootstrap
 make install
