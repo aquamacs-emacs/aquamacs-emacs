3bdd41f1e821f0722eedc30fdf2b10078ca4f9a1
David Reitter <david.reitter@gmail.com>
2012-08-08 19:28:21 -0400

    emulate-mac-keyboard-mode: now licensed under the GPL

--- aquamacs/src/site-lisp/macosx/emulate-mac-keyboard-mode.el
+++ aquamacs/src/site-lisp/macosx/emulate-mac-keyboard-mode.el
@@ -1,8 +1,26 @@
 ;; -*-mode:emacs-lisp; coding: utf-8;-*-'
 ;; emulate-mac-*-keyboard-modes for Aquamacs
-;; (C) 2005, 2007, 2010, 2011 by David Reitter
-;; do not copy / redistribute outside of Aquamacs. All rights reserved.
 
+
+;; Aquamacs Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; Aquamacs Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;; (C) 2005, 2007, 2010, 2011, 2012 by David Reitter
+
+;;
+;;
 ;; This defines multiple global minor modes, each of which
 ;; emulates common keys of a keyboard layout.
 ;;
