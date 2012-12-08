cf8151fcd21e50a2df13f6c0c0de640b7b4f56bf
David Reitter <david.reitter@gmail.com>
2012-12-08 00:59:12 -0500

    copy-build script: fix last commit

--- aquamacs/build/copy-build-to-server.sh
+++ aquamacs/build/copy-build-to-server.sh
@@ -13,7 +13,7 @@ CHGLOGSCRIPT=~/aquamacs-web/scripts/push-nightly-changelog.sh
 SOURCE=`pwd`/builds
 LOGPATH=`pwd`
 
-DEST=dr@cc:/Sites/Aquamacs/24
+DEST=dr@cc:Sites/Aquamacs/24
 
 TMP=/tmp/builds
 
