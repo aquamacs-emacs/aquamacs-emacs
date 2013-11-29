aa32e0f0f57db572ea5c138f3ec32cd90913e006
David Reitter <david.reitter@gmail.com>
2013-11-29 16:08:56 -0500

    increase version number to 3.0preview update changelog

--- aquamacs/src/site-lisp/site-start.el
+++ aquamacs/src/site-lisp/site-start.el
@@ -37,7 +37,7 @@
 
 ;; Version information
 
-(defvar aquamacs-version "3.x"
+(defvar aquamacs-version "3.0preview"
 "A string with Aquamacs' version number.
 The format of the string is undefined. 
 For a reliable numerical representation, use `aquamacs-version-id'.")
@@ -51,7 +51,7 @@ It is guaranteed that for any two Aquamacs releases A and B,
 if aquamacs-version-id for B is higher than aquamacs-version-id 
 for A, then B is newer than A.")
 
-(defvar aquamacs-minor-version "dev"
+(defvar aquamacs-minor-version ""
 "Version code for minor maintenance releases.
 Changes in this code are ignored during the online version check.")
 
