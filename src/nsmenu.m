8038995032aabfbd8da0cedb8fa85d781d249ab6
David Reitter <david.reitter@gmail.com>
2013-12-04 00:44:44 -0500

    addDisplayItemWithImage - use stringWithCString:encoding:

--- src/nsmenu.m
+++ src/nsmenu.m
@@ -1556,8 +1556,8 @@ Items in this list are always Lisp symbols.*/)
 {
 
 
-  NSString *label_str = [NSString stringWithUTF8String: label];
-  NSString *help_str = [NSString stringWithUTF8String: help];
+  NSString *label_str = [NSString stringWithCString: label encoding:NSASCIIStringEncoding];
+  NSString *help_str = [NSString stringWithCString: help encoding:NSASCIIStringEncoding];
 
   /* 1) come up w/identifier */
   // NSString *identifier
