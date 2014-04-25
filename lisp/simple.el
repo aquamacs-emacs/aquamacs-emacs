e61baa923cba77588a5abcff4e5cb1d3551bc46e
David Reitter <david.reitter@gmail.com>
2014-04-24 20:18:09 -0400

    smart-spacing-end-of-line: deal with narrowed buffers

--- lisp/simple.el
+++ lisp/simple.el
@@ -8032,7 +8032,9 @@ the point is when the command is called.")
   `(not (string= (substring (buffer-name ,buf) 0 1) " ")))
 
 (defsubst smart-spacing-end-of-line (pos)
-  (equal "\n" (buffer-substring-no-properties (max (point-min) (1- pos)) pos)))
+  (and (<= pos (point-max)) ;; buffer may be narrowed
+       (> pos (point-min))
+       (equal "\n" (buffer-substring-no-properties  (1- pos) pos))))
 
 (defun smart-spacing-filter-buffer-substring (beg end &optional delete)
   "Like `filter-buffer-substring', but add spaces around content if region is a phrase."
