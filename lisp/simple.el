5ca311b85bc9efc5b2817b43db0a21c207952624
Phillip Lord <phillip.lord@newcastle.ac.uk>
2015-09-11 10:57:57 +0100

    New variable "undo-buffer-undoably-changed".

--- lisp/simple.el
+++ lisp/simple.el
@@ -2215,6 +2215,12 @@ A redo record for ordinary undo maps to the following (earlier) undo.")
 (defvar undo-no-redo nil
   "If t, `undo' doesn't go through redo entries.")
 
+(defvar-local undo-buffer-undoably-changed nil
+  "Non-nil means that that the buffer has had a recent undo-able change.
+
+Recent means since the value of this variable was last set explicitly to nil,
+usually as part of the undo machinary.")
+
 (defvar pending-undo-list nil
   "Within a run of consecutive undo commands, list remaining to be undone.
 If t, we undid all the way to the end of it.")
