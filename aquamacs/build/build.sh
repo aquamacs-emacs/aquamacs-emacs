224101cba51729262821ddd9cc3bc7140cd14e73
David Reitter <david.reitter@gmail.com>
2015-11-09 22:06:43 -0500

    Use old SDK for nightly builds and releases
    Set SDKROOT to oldest available SDK
    This will provide backwards binary compatibility and avoid
    dynamically linking against libraries that happen to be available
    in the build environment.

--- aquamacs/build/build.sh
+++ aquamacs/build/build.sh
@@ -9,6 +9,7 @@ export PATH=$AUTOTOOLS:$PATH
 OMIT_AUTOGEN=1
 FLAGS=
 OMIT_SYMB=1
+OLD_SDK=0
 
 case "$1" in
 '-release')
@@ -19,6 +20,7 @@ case "$1" in
   OMIT_AUTOGEN=
   FLAGS='-arch x86_64 -O3 -g -mtune=corei7'
   OMIT_SYMB=
+  OLD_SDK=1
   ;;
 '-nightly')
   # do not use MacPorts / fink libraries for binary compatibility
@@ -28,6 +30,7 @@ case "$1" in
   OMIT_AUTOGEN=
   FLAGS="-arch x86_64 -O3 -g -mtune=corei7 $FLAGS"
   OMIT_SYMB=
+  OLD_SDK=1
   ;;
 *)
   # during development, do not compress .el files to speed up "make install"
@@ -45,22 +48,47 @@ echo "Compiler flags: $FLAGS"
 # do not use MacPorts / fink libraries
 # do not use binaries either (e.g., gnutls would be recognized)
 
+# We will run only on 10.6 and later.
 MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-"10.6"}
 export MACOSX_DEPLOYMENT_TARGET
-SDKROOT=${SDKROOT:-"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk"}
-export SDKROOT
 
-echo "MACOSX_DEPLOYMENT_TARGET=" $MACOSX_DEPLOYMENT_TARGET
+FINALMESSAGE=""
+
+if test $OLD_SDK -gt 0;
+then
+# we're going to choose the oldest SDK we have (starting with 10.9)
+# this should guarantee backwards compatibility up to that SDK version.
+# for current Aquamacs, this will typically be 10.9
+for VERS in 10.6 10.7 10.8 10.9 10.10 10.11 10.12 10.13 10.14; do
+    SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${VERS}.sdk"
+    echo $SDK
+    if [ -d "$SDK" ]; then
+        FINALMESSAGE="This build will be compatible with OS X $VERS onwards."
+	SDKROOT=${SDKROOT:-"$SDK"}
+	break
+    fi
+done
+export SDKROOT
+else
+    FINALMESSAGE="Binary backwards compatibility not available."
+fi
+
+echo "PATH=" $PATH
 echo "SDKROOT=" $SDKROOT
+echo "MACOSX_DEPLOYMENT_TARGET=" $MACOSX_DEPLOYMENT_TARGET
+
+
+# Note: Setting MACOSX_DEPLOYMENT_TARGET is likely to be sufficient.
 
 # autoconf must be run via macports to allow its upgrade
 test $OMIT_AUTOGEN || ./autogen.sh ; \
 ./configure --with-ns --without-x CFLAGS="$FLAGS -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET" LDFLAGS="$FLAGS -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"; \
 make clean ; \
-make all ; \
-make install ; \
+make -j4 all && make -j2 install ; \
 rm -f etc/DOC-* ; \
-
 
 # generate symbol archive
 test $OMIT_SYMB || dsymutil src/emacs
+
+echo ${FINALMESSAGE}
+echo "Build finished."
