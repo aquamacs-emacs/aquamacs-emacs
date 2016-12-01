5cd46bc1747b00fdd0490d143bad65f210f40798
Hayo Baan <info@hayobaan.com>
2016-12-01 09:29:54 +0100

    Got rid of undefined warning group warning when compiling on Mac OS

--- src/keyboard.c
+++ src/keyboard.c
@@ -70,9 +70,11 @@ along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.  */
 #include TERM_HEADER
 #endif /* HAVE_WINDOW_SYSTEM */
 
-/* Work around GCC bug 54561.  */
+/* Work around GCC bug 54561 (but not for Mac as -Wclobbered isn't defined there) */
+#ifndef DARWIN_OS
 #if GNUC_PREREQ (4, 3, 0)
 # pragma GCC diagnostic ignored "-Wclobbered"
+#endif
 #endif
 
 /* Variables for blockinput.h:  */
