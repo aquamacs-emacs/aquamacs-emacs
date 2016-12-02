2852e7240c922cc4e997a7f77145203d1323e5a7
Hayo Baan <info@hayobaan.com>
2016-12-02 18:27:16 +0100

    Removed deprecated method
    Since we’re on 10.6+, the method bestRepresentationForRect is always
    available so the code that checks this is unnecessary. This also gets
    rid of the deprecated (and unused) call to bestRepresentationForDevice.

--- src/nsimage.m
+++ src/nsimage.m
@@ -190,15 +190,8 @@ ns_resize_truedpi_image (void *eImg, int respect_dpi, Lisp_Object scale_factor, 
   
   // we'll optimize for the main screen.
   // needed to pick the right representation e.g., when HiDPI image is provided.
-  if ([image respondsToSelector: @selector (bestRepresentationForRect:context:hints:)])
-    {
-      // Choose the smallest (full-resolution) image representation
-      imgRep = [image bestRepresentationForRect: NSMakeRect(100,100,2,2)  context:nil hints: nil];
-    }
-  else
-    {
-      imgRep = [image bestRepresentationForDevice: nil];
-    }
+  // Choose the smallest (full-resolution) image representation
+  imgRep = [image bestRepresentationForRect: NSMakeRect(100,100,2,2)  context:nil hints: nil];
   
   if (imgRep == nil)
       return;
