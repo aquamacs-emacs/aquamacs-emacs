5583839f60f64aca40aea9526558f3533974442d
David Reitter <david.reitter@gmail.com>
2018-07-23 17:20:26 -0500

    Update Aquamacs manual

--- aquamacs/doc/Aquamacs Help/internals.pl
+++ aquamacs/doc/Aquamacs Help/internals.pl
@@ -1,4 +1,4 @@
-# LaTeX2HTML 2008 (1.71)
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
 # Associate internals original text with physical files.
 
 
@@ -6,16 +6,20 @@ $key = q/Look/;
 $ref_files{$key} = "$dir".q|node17.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/frame-appearance-styles/;
-$ref_files{$key} = "$dir".q|node17.html|; 
+$key = q/aquamacs-screenshot.jpg/;
+$ref_files{$key} = "$dir".q|node2.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/aquamacs-tex/;
 $ref_files{$key} = "$dir".q|node25.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/tab:terms/;
-$ref_files{$key} = "$dir".q|node6.html|; 
+$key = q/frame-appearance-styles/;
+$ref_files{$key} = "$dir".q|node17.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/standardpaths/;
+$ref_files{$key} = "$dir".q|node32.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/tab:command/;
@@ -26,16 +30,12 @@ $key = q/tab:shortcuts/;
 $ref_files{$key} = "$dir".q|node23.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/tab:terms/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/tab:variables/;
 $ref_files{$key} = "$dir".q|node24.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/aquamacs-screenshot.jpg/;
-$ref_files{$key} = "$dir".q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/standardpaths/;
-$ref_files{$key} = "$dir".q|node32.html|; 
 $noresave{$key} = "$nosave";
 
 1;
--- aquamacs/doc/Aquamacs Help/labels.pl
+++ aquamacs/doc/Aquamacs Help/labels.pl
@@ -1,10 +1,6 @@
-# LaTeX2HTML 2008 (1.71)
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
 # Associate labels original text with physical files.
 
-
-$key = q/frame-appearance-styles/;
-$external_labels{$key} = "$URL/" . q|node17.html|; 
-$noresave{$key} = "$nosave";
 
 $key = q/Look/;
 $external_labels{$key} = "$URL/" . q|node17.html|; 
@@ -12,6 +8,14 @@ $noresave{$key} = "$nosave";
 
 $key = q/aquamacs-screenshot.jpg/;
 $external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/aquamacs-tex/;
+$external_labels{$key} = "$URL/" . q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/frame-appearance-styles/;
+$external_labels{$key} = "$URL/" . q|node17.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/standardpaths/;
@@ -26,10 +30,6 @@ $key = q/tab:shortcuts/;
 $external_labels{$key} = "$URL/" . q|node23.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/aquamacs-tex/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/tab:terms/;
 $external_labels{$key} = "$URL/" . q|node6.html|; 
 $noresave{$key} = "$nosave";
@@ -41,23 +41,19 @@ $noresave{$key} = "$nosave";
 1;
 
 
-# LaTeX2HTML 2008 (1.71)
+# LaTeX2HTML 2018 (Released Feb 1, 2018)
 # labels from external_latex_labels array.
 
 
-$key = q/tab:variables/;
-$external_latex_labels{$key} = q|4|; 
+$key = q/Look/;
+$external_latex_labels{$key} = q|3.5.4|; 
 $noresave{$key} = "$nosave";
 
-$key = q/tab:shortcuts/;
-$external_latex_labels{$key} = q|2|; 
+$key = q/_/;
+$external_latex_labels{$key} = q|<|; 
 $noresave{$key} = "$nosave";
 
-$key = q/tab:command/;
-$external_latex_labels{$key} = q|3|; 
-$noresave{$key} = "$nosave";
-
-$key = q/tab:terms/;
+$key = q/aquamacs-screenshot.jpg/;
 $external_latex_labels{$key} = q|1|; 
 $noresave{$key} = "$nosave";
 
@@ -65,20 +61,28 @@ $key = q/aquamacs-tex/;
 $external_latex_labels{$key} = q|2|; 
 $noresave{$key} = "$nosave";
 
+$key = q/frame-appearance-styles/;
+$external_latex_labels{$key} = q|3.5.4|; 
+$noresave{$key} = "$nosave";
+
 $key = q/standardpaths/;
 $external_latex_labels{$key} = q|6.2|; 
 $noresave{$key} = "$nosave";
 
-$key = q/aquamacs-screenshot.jpg/;
+$key = q/tab:command/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:shortcuts/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:terms/;
 $external_latex_labels{$key} = q|1|; 
 $noresave{$key} = "$nosave";
 
-$key = q/Look/;
-$external_latex_labels{$key} = q|3.5.4|; 
-$noresave{$key} = "$nosave";
-
-$key = q/frame-appearance-styles/;
-$external_latex_labels{$key} = q|3.5.4|; 
+$key = q/tab:variables/;
+$external_latex_labels{$key} = q|4|; 
 $noresave{$key} = "$nosave";
 
 1;
