c194705c76e87ce42fd74139290a4ea9545bbd39
David Reitter <david.reitter@gmail.com>
2018-10-02 17:52:35 -0400

    increase version number to 3.5

--- aquamacs/src/site-lisp/site-start.el
+++ aquamacs/src/site-lisp/site-start.el
@@ -38,12 +38,12 @@
 
 ;; Version information
 
-(defvar aquamacs-version "3.4"
+(defvar aquamacs-version "3.5"
 "A string with Aquamacs' version number.
 The format of the string is undefined. 
 For a reliable numerical representation, use `aquamacs-version-id'.")
 
-(defvar aquamacs-version-id 310
+(defvar aquamacs-version-id 311
 "A float indicating Aquamacs' version number.
 Full integers correspond to the third position of the public
 version number, e.g. version 0.9.7 is represented as `97.x'.
