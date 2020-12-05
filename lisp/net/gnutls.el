dc560d429f0fb5ed86233ad4f1142ada8ba72e43
Win Treese <treese@acm.org>
2020-12-04 20:20:49 -0500

    Update gnutls configuration for how the net works now (bug #205)
    The changes are in gnutls.el because it's easy and because they
    should not be necessary after updating to a later version of Emacs.

--- lisp/net/gnutls.el
+++ lisp/net/gnutls.el
@@ -43,7 +43,7 @@
   :prefix "gnutls-"
   :group 'comm)
 
-(defcustom gnutls-algorithm-priority nil
+(defcustom gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"
   "If non-nil, this should be a TLS priority string.
 For instance, if you want to skip the \"dhe-rsa\" algorithm,
 set this variable to \"normal:-dhe-rsa\"."
@@ -92,6 +92,7 @@ to all of the tests described above."
     "/etc/ssl/ca-bundle.pem"                 ; Suse
     "/usr/ssl/certs/ca-bundle.crt"           ; Cygwin
     "/usr/local/share/certs/ca-root-nss.crt" ; FreeBSD
+    "/etc/ssl/cert.pem"                      ; macOS, Dragora, Parabola
     )
   "List of CA bundle location filenames or a function returning said list.
 The files may be in PEM or DER format, as per the GnuTLS documentation.
