f82c1a28ff6a0f0ca234df435a68d24af7d908d7
Win Treese <treese@acm.org>
2021-03-20 20:43:39 -0400

    Add more examples of possible debug options

--- aquamacs/build/build-debug.sh
+++ aquamacs/build/build-debug.sh
@@ -4,8 +4,26 @@
 # Should be run as
 #     aquamacs/build/build-debug.sh
 # from the top level of the Aquamacs source tree.
+#
+# This is an example of configuring for debugging, with some of the
+# possible options for both configuration and compiler flags.
+
+# This is intended to be tweaked for whatever debugging is needed.
 
 # Options for debugging: one for ./configure, one for CFLAGS
+
+# Turn on assertion checking: --enable-checking
+
+# It would be nice to use this, but it looks like there are a lot of
+# problems with assertions that are not shedding light on the main
+# bugs now, and they also seem to be in areas that have substantial
+# changes in Emacs 27/28.
+#export DEBUG_CONFIG_OPTS="--enable-checking"
+
+# DEBUG_CONFIG_OPTS="--enable-checking='yes,glyphs'"
+# DEBUG_CONFIG_OPTS+" -enable-check-lisp-object-type"
+DEBUG_CFLAGS='-O0 -g3'
+
 
 NS_TRACE_FLAGS="-DNSTRACE_ENABLED=1"
 NS_TRACE_FLAGS+=" -DNSTRACE_ALL_GROUPS=1"
@@ -19,17 +37,8 @@ NS_TRACE_FLAGS+=" -DNSTRACE_GROUP_FOCUS=1"
 # Override NS_TRACE_FLAGS for now
 NS_TRACE_FLAGS=
 
-# Turn on assertion checking: --enable-checking
-
-# It would be nice to use this, but it looks like there are a lot of
-# problems with assertions that are not shedding light on the main
-# bugs now, and they also seem to be in areas that have substantial
-# changes in Emacs 27/28.
-
-#export DEBUG_CONFIG_OPTS="--enable-checking"
 export DEBUG_CONFIG_OPTS=""
 export DEBUG_CFLAGS="-O0 -g3 ${NS_TRACE_FLAGS} -fno-omit-frame-pointer"
-
 
 export MIN_VERSION=
 ./build-aquamacs
