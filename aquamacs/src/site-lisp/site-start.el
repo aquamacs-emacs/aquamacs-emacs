db6bd747ee964b24ad5a2b11f598cb91c6bb60ea
Win Treese <treese@acm.org>
2021-06-05 23:51:48 -0400

    Updated version information

--- aquamacs/src/site-lisp/site-start.el
+++ aquamacs/src/site-lisp/site-start.el
@@ -38,7 +38,7 @@
 
 ;; Version information
 
-(defvar aquamacs-version "3.6-beta"
+(defvar aquamacs-version "3.6"
 "A string with Aquamacs' version number.
 The format of the string is undefined.
 For a reliable numerical representation, use `aquamacs-version-id'.")
@@ -52,7 +52,7 @@ It is guaranteed that for any two Aquamacs releases A and B,
 if aquamacs-version-id for B is higher than aquamacs-version-id
 for A, then B is newer than A.")
 
-(defvar aquamacs-minor-version "beta"
+(defvar aquamacs-minor-version "-beta"
   "Version code for minor maintenance releases.
 Changes in this code are ignored during the online version check.")
 
